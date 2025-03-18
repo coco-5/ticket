8bcb03040164b39ff6976368721b432e66e1ee5b
wujingtan <wujingtan@yy.com>
2025-03-18 15:29:17 +0800

    6

--- src/packageBook/components/date.vue
+++ src/packageBook/components/date.vue
@@ -1,33 +1,160 @@
 <template>
     <view class="c-date">
-        <view class="wrap">
-            <view class="item"></view>
+        <scroll-view 
+            class="wrap"
+            scroll-x
+        >
+            <view 
+                class="item"
+                :class="current == index ? 'on' : ''"
+                v-for="(item,index) in daysList"
+                :key="index"
+            >
+                <view class="date">{{item.month}}-{{item.date}}</view>    
+                <view class="week">
+                    <template v-if="item.isToday">今天</template>
+                    <template v-else>
+                        周{{item.weekDayText}}
+                    </template>
+                </view>
+            </view>
+        </scroll-view>
+        <view 
+            class="ico"
+            @click="isShowDatePop = true"
+        >
+            <view class="calc"></view>
+            <view class="text">日历</view>
         </view>
-        <view class="ico"></view>
     </view>
 </template>
 
 <script>
+import utils from '@/utils/utils'
 export default{
     props:{
-
+        options:{
+            type:Object,
+            default:{}
+        }
     },
     data(){
         return{
-            now:new Date()
+            today:new Date(),
+            daysList:[],
+            current:0,
+            isShowDatePop:false
         }
     },
     mounted(){
-
+        this.initDate()
     },
     methods:{
+        initDate(){
+            let today = this.today
+            let nextMonth = new Date(today)
+            let daysList = []
+            let weekDayText = ['日', '一', '二', '三', '四', '五', '六']
 
+            nextMonth.setMonth(today.getMonth() + 1)
+
+            let timeDifference = nextMonth.getTime() - today.getTime()
+            let daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24))
+            let current = 0
+
+            for(let i=0; i<=daysDifference; i++){
+                let day = new Date(today.getTime() + i * 24 * 60 * 60 * 1000)
+
+                let isToday = 
+                    day.getFullYear() === today.getFullYear() &&
+                    day.getMonth() === today.getMonth() &&
+                    day.getDate() === today.getDate()
+                
+                if(this.options.date){
+                          
+                }
+
+                daysList.push({
+                    month:utils.formatNumber(day.getMonth()+1),
+                    date:utils.formatNumber(day.getDate()),
+                    weekDayText:weekDayText[day.getDay()],
+                    isToday:isToday
+                })
+            }
+
+            this.daysList = daysList
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
 .c-date {
-    
+    position:relative;
+    padding-right:168rpx;
+    height:80rpx;
+    .wrap {
+        width:100%;
+        height:80rpx;
+        white-space:nowrap;
+        overflow-x:auto;
+        .item {
+            display:inline-block;
+            margin-left:36rpx;
+            width:85rpx;
+            height:80rpx;
+            color:#8A8A8A;
+            font-size:18rpx;
+            text-align:center;
+            vertical-align:top;
+            overflow:hidden;
+            .date,
+            .week {
+                height:22rpx;
+                line-height:22rpx;
+            }
+            .date {
+                margin:20rpx 0 0;
+            }
+            &.on {
+                background:linear-gradient(123deg, #FFA63F, #EB5628);
+                border-radius:20rpx;
+                color:#FFF;
+            }
+        }
+    }
+    .ico {
+        position:absolute;
+        top:0;
+        right:0;
+        padding-left:26rpx;
+        width:94rpx;
+        height:80rpx;
+        &::before {
+            content:' ';
+            position:absolute;
+            top:50%;
+            left:-26rpx;
+            transform:translateY(-50%);
+            width:26rpx;
+            height:74rpx;
+            background:url('http://182.254.192.167:6003/vue/upload/static/ticket/icon-time-left.png') no-repeat;
+            background-size:contain;
+        }
+        .calc {
+            margin:5rpx auto;
+            width:30rpx;
+            height:30rpx;
+            background:url('http://182.254.192.167:6003/vue/upload/static/ticket/icon-time.png') no-repeat;
+            background-size:contain;
+        }
+        .text {
+            height:24rpx;
+            line-height:24rpx;
+            color:#000;
+            font-size:18rpx;
+            text-align:center;
+        }
+    }
 }
 </style>
--- src/packageBook/pages/flight/one.vue
+++ src/packageBook/pages/flight/one.vue
@@ -6,7 +6,9 @@
             <view class="text">澳门氹仔</view>
         </view>
 
-        <view class="dates"></view>
+        <view class="dates">
+            <date></date>
+        </view>
 
         <view class="list">
             <view class="item"></view>
@@ -39,5 +41,29 @@ export default {
 .page {
     min-height:100vh;
     background:#F8F8F8;
+}
+
+.destination {
+    padding:32rpx 0;
+    color:#000;
+    font-size:34rpx;
+    font-weight:500;
+    text-align:center;
+    .text,
+    .ico {
+        display:inline-block;
+        vertical-align:middle;
+    }
+    .ico {
+        margin:0 16rpx;
+        width:40rpx;
+        height:5rpx;
+        background:url('http://182.254.192.167:6003/vue/upload/static/ticket/car2.png') no-repeat;
+        background-size:contain;
+    }
+}
+
+.dates {
+    margin:0 0 32rpx;
 }
 </style>
