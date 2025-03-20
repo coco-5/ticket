6263c392d54042b7a71560f9add64264265acfe9
wujingtan <wujingtan@yy.com>
2025-03-20 11:26:24 +0800

    9

---
+++ src/packageBook/components/card.vue
@@ -0,0 +1,148 @@
+<template>
+    <view class="c-card">
+        <view class="date">
+            <view class="date-item">
+                <view class="d">08:00</view>
+                <view class="t">蛇口港口</view>
+            </view>
+            <view class="long">
+                <view class="t">1小时40分</view>
+                <view class="ico"></view>
+            </view>
+            <view class="date-item">
+                <view class="d">08:00</view>
+                <view class="t">蛇口港口</view>
+            </view>
+        </view>
+        <view class="grade">
+            <view class="i">
+                普通舱<text>（49张）</text>
+            </view>
+            <view class="i">
+                头等舱<text>（49张）</text>
+            </view>
+        </view>
+        <div class="price">
+            <view class="price-item mop">
+                <text class="t">MOP</text>
+                <text class="p">65</text>
+                <text class="q">起</text>
+            </view>
+            <view class="price-item">
+                <text class="t">RMB</text>
+                <text class="p">65</text>
+            </view>
+        </div>
+    </view>
+</template>
+
+<script>
+export default {
+    props:{
+        item:{
+            type:Object,
+            default:[]
+        }
+    },
+    data(){
+        return{
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.c-card {
+    position:relative;
+    margin-top:-1px;
+    padding:0 24rpx;
+    height:200rpx;
+    border-top:1px solid #E5E5E5;
+    overflow:hidden;
+    .date {
+        margin:20rpx 0 8rpx 0;
+        white-space:nowrap;
+        text-align:center;
+        .date-item,
+        .long {
+            display:inline-block;
+            text-align:center;
+        }
+        .date-item {
+            width:80rpx;
+            vertical-align:middle;
+            .d {
+                height:40rpx;
+                line-height:40rpx;
+                color:#000;
+                font-size:30rpx;
+                font-weight:500;
+            }
+            .t {
+                color:#8D8D8D;
+                font-size:20rpx;
+            }
+        }
+        .long {
+            margin:0 24rpx 0;
+            vertical-align:16rpx;
+            .t {
+                margin-bottom:8rpx;
+                height:24rpx;
+                line-height:24rpx;
+                color:#A3A3A3;
+                font-size:18rpx;
+            }
+            .ico {
+                width:91rpx;
+                height:9rpx;
+                background:url('http://182.254.192.167:6003/vue/upload/static/round/ico-arrow2.png') no-repeat;
+                background-size:contain;
+            }
+        }
+    }
+    .grade {
+        .i {
+            display:inline-block;
+            color:#A3A3A3;
+            font-size:20rpx;
+            vertical-align:middle;
+            text {
+                color:#EC702E;
+            }
+        }
+    }
+    .price {
+        height:36rpx;
+        text-align:right;
+        .price-item {
+            display:inline-block;
+            margin-left:40rpx;
+            height:45rpx;
+            line-height:45rpx;
+            color:#000;
+            font-size:20rpx;
+            vertical-align:middle;
+            text {
+                display:inline-block;
+            }
+            .p {
+                margin:0 4rpx;
+                font-size:30rpx;
+                font-weight:500;
+            }
+            .t {
+                font-weight:500;
+            }
+            .q {
+                color:#A3A3A3;
+            }
+            &.mop {
+                color:#FE6630;
+            }
+        }
+    }
+}
+</style>
--- src/packageBook/pages/flight/round.vue
+++ src/packageBook/pages/flight/round.vue
@@ -1,19 +1,55 @@
 <template>
     <view class="page">
         <destination></destination>
-        round 1
+        <div class="list">
+            <div 
+                class="item-list"
+                v-if="listDeparture.length"
+            >
+                <view class="hd">
+                    <view class="text">去程 2024-06-12</view>
+                    <view class="ico"></view>
+                </view>
+                <view class="bd">
+                    <card
+                        :item="item"
+                        v-for="(item,index) in listDeparture"
+                        :key="index"
+                    ></card>
+                </view>
+            </div>
+            <div 
+                class="item-list arrival"
+                v-if="listArrival.length"
+            >
+                <view class="hd">
+                    <view class="text">返程 2024-06-13</view>
+                    <view class="ico"></view>
+                </view>
+                <view class="bd">
+                    <card
+                        :item="item"
+                        v-for="(item,index) in listArrival"
+                        :key="index"
+                    ></card>
+                </view>
+            </div>
+        </div>
     </view>
 </template>
 
 <script>
 import destination from '@/packageBook/components/destination'
+import card from '@/packageBook/components/card'
 export default {
     components:{
-        destination
+        destination,
+        card
     },
     data(){
         return{
-
+            listDeparture:[{},{}],
+            listArrival:[{},{}]
         }
     },
     onLoad(e){
@@ -29,5 +65,45 @@ export default {
 .page {
     min-height:100vh;
     background:#F8F8F8;
+}
+
+.list {
+    margin:0 17rpx;
+    .item-list {
+        display:inline-block;
+        width:354rpx;
+        vertical-align:top;
+        .hd {
+            height:83rpx;
+            line-height:83rpx;
+            background:url('http://182.254.192.167:6003/vue/upload/static/round/tab1.png') no-repeat;
+            background-size:contain;
+            color:#FFF;
+            font-size:30rpx;
+            text-align:center;
+            .text,
+            .ico {
+                display:inline-block;
+                vertical-align:middle;
+            }
+            .ico {
+                margin-left:8rpx;
+                width:11rpx;
+                height:19rpx;
+                background:url('http://182.254.192.167:6003/vue/upload/static/round/ico-arrow.png') no-repeat;
+                background-size:contain;
+            }
+        }
+        &.arrival {
+            margin-left:10rpx;
+            .hd {
+                background-image:url('http://182.254.192.167:6003/vue/upload/static/round/tab2.png');
+            }
+        }
+        .bd {
+            background:#FFF;
+            border-radius:0 0 20rpx 20rpx;
+        }
+    }
 }
 </style>
